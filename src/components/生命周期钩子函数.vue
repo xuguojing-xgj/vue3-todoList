0d731377437253c92bd3ef66523b497e77f69e96
xuguojing-xgj <2313493661@qq.com>
2022-07-20 13:45:47 +0800

    解决ts警告 命名规范问题

---
+++ src/components/生命周期钩子函数.vue
@@ -0,0 +1,20 @@
+<template>
+    <div></div>
+    <div></div>
+    <div></div>
+</template>
+
+<script >
+import { onMounted } from 'vue'
+export default {
+    name: 'app',
+    setup() {
+        onMounted(() => {
+            console.log('mounted执行了');
+        })
+    }
+}
+</script>
+
+<style>
+</style>
