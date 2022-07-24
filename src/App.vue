2f94628af7aa44d2a5f953137fe055ffdfdba13c
xuguojing-xgj <2313493661@qq.com>
2022-07-24 13:22:25 +0800

    生命周期钩子函数

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <atemp></atemp>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import atemp from './components/生命周期钩子函数.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        atemp
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
